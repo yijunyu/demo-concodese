d48208d73109955ce9802ed5a202d4b541a0e090
Yijun Yu <y.yu@open.ac.uk>
2019-03-19 20:32:31 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 #from laslabs/alpine-jira
-from cptactionhank/atlassian-jira:latest
+#from cptactionhank/atlassian-jira:latest
+from cptactionhank/atlassian-jira-software:latest
 USER root
 RUN mkdir -p /concodese 
 COPY bin /concodese/bin
 COPY config /concodese/config
+COPY projects /concodese/projects
+COPY log /concodese/log
+COPY sample /concodese/sample
+COPY doc /concodese/doc
--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ vid=$(docker volume inspect jiradb)
 if [ "$?" = "1" ]; then
 	docker volume create jiradb
 fi
-docker run --detach --mount source=jiradb,destination=/var/atlassian/jira -p 8080:8080 yijun/demo-concodese
+docker run --detach --mount source=jiradb,destination=/var/atlassian/jira -p 8080:8080 -p 8081:8081 yijun/demo-concodese
 id=$(docker ps --filter "publish=8080"  --format "{{.ID}}")
 docker exec -it $id /bin/bash
