365b3fc6d74fe703e0c4765e53a911461658c58c
Yijun Yu <y.yu@open.ac.uk>
2019-03-20 14:58:17 +0000

    separate the two services

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
-#from laslabs/alpine-jira
-#from cptactionhank/atlassian-jira:latest
-from cptactionhank/atlassian-jira-software:latest
+from yijun/fast:latest
 USER root
 RUN mkdir -p /concodese 
 COPY bin /concodese/bin
@@ -9,3 +7,7 @@ COPY projects /concodese/projects
 COPY log /concodese/log
 COPY sample /concodese/sample
 COPY doc /concodese/doc
+RUN cd /concodese/bin && ./UpdateProjectConfigData.sh
+WORKDIR /concodese/bin
+EXPOSE 8081
+ENTRYPOINT [ "/usr/bin/java", "-cp", "/concodese/config/lic:/concodese/bin/ConCodeSe-1.0.0.jar", "com.concodese.ConCodeSeJettyServerStarter", "SERVER_PORT=8081"]
--- run.sh
+++ run.sh
@@ -3,7 +3,15 @@ vid=$(docker volume inspect jiradb)
 if [ "$?" = "1" ]; then
 	docker volume create jiradb
 fi
-docker run --detach --mount source=jiradb,destination=/var/atlassian/jira -p 8080:8080 -p 8081:8081 yijun/demo-concodese
 id=$(docker ps --filter "publish=8080"  --format "{{.ID}}")
-docker exec -it $id /bin/bash -c 'cd /concodese/bin && ./UpdateProjectConfigData.sh'
-docker exec -it $id /bin/bash -c 'cd /concodese/bin && ./concodese_service.sh start SERVER_PORT=8081'
+if [ "$id" != "" ]; then
+	docker kill $id
+fi
+id=$(docker ps --filter "publish=8081"  --format "{{.ID}}")
+if [ "$id" != "" ]; then
+	docker kill $id
+fi
+docker run --detach --mount source=jiradb,destination=/var/atlassian/jira -p 8080:8080 cptactionhank/atlassian-jira-software:latest
+docker run --detach -p 8081:8081 yijun/demo-concodese
+id=$(docker ps --filter "publish=8081"  --format "{{.ID}}")
+docker exec -it $id /bin/bash
