b84801fc15c6269e1b7d69c5e2cc32fafebeb66b
Yijun Yu <y.yu@open.ac.uk>
2019-05-08 16:08:25 +0100

    fixing permission problem

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ if [ "$id" != "" ]; then
 fi
 docker network prune
 docker network create mynet
-docker run --rm --name jira --net mynet --detach -v $(pwd)/jiradb:/var/atlassian/jira -p 8080:8080 cptactionhank/atlassian-jira-software:latest
-docker run --rm --name concodese --net mynet --detach -v /tmp:/tmp -v ${1:-$(pwd)}:/projects -w /projects -p 8081:8081 yijun/demo-concodese
+docker run --rm --privileged=true --name jira --net mynet --detach -v $(pwd)/jiradb:/var/atlassian/jira -p 8080:8080 cptactionhank/atlassian-jira-software:latest
+docker run -u $(id -u):$(id -g) --rm --name concodese --net mynet --detach -v /tmp:/tmp -v ${1:-$(pwd)}:/projects -w /projects -p 8081:8081 yijun/demo-concodese
 id=$(docker ps --filter "publish=8081"  --format "{{.ID}}") 
 docker exec -it $id /bin/bash
